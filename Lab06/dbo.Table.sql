410632242767717395379eb54c3e7b1055f65f7a
aikyh <aikyh02070208@gmail.com>
2024-12-23 21:56:02 +0800

    Revert "Add project files."
    This reverts commit 3af49c071aff12a391987eaaef0955348e19744e.

--- Lab06/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Products]
-( 
-    [Product_ID] NCHAR(50) PRIMARY KEY NOT NULL, 
-    [Product_Name] NCHAR(50) NOT NULL, 
-    [Product_Desc] VARCHAR(50) NOT NULL, 
-    [Unit_Price] DECIMAL(18, 2) NOT NULL, 
-    [Stock_Level] INT NOT NULL
-)
